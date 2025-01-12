f62f7b35fbfbb249d2d7a81616474bc57c5166a7
adr <rodrigues.adriano2607@gmail.com>
2025-01-12 01:50:09 -0300

    adiciona carrossel de slides com react-slick e ajusta estilos para apresentação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ import React from "react";
 import CardUI from "@/components/CardUI";
 import { DataItem } from "@/interface/int";
 import CircularProgressUI from "@/components/CircularProgress";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 
 export default function Home() {
@@ -31,39 +36,79 @@ export default function Home() {
     fetchData();
   }, []);
 
+  const sliderSettings = {
+    dots: false,
+    infinite: false,
+    speed: 1000,
+    slidesToShow: 5, // Número de slides padrão
+    slidesToScroll: 5,
+    autoplay:false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024, // Até 1024px (tablets)
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768, // Até 768px (dispositivos móveis)
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480, // Até 480px (celulares pequenos)
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   
   return (
   
   
       <section className="container mx-auto">
-      <div className="flex justify-center items-center min-h-[70vh]">
+     
         {data.length > 0 ? (
+          <>
+          <div className="mb-5">
+            <Slider {...sliderSettings}>
+              {data
+                .filter((a) => a.role === "survivor")
+                .map((item, index) => (
+                  <div key={index}>
+                    <CardUI item={item} />
+                  </div>
+                ))}
+            </Slider>
+          </div>
+        
+          <div>
+            <Slider {...sliderSettings}>
+              {data
+                .filter((a) => a.role === "killer")
+                .map((item, index) => (
+                  <div key={index}>
+                    <CardUI item={item} />
+                  </div>
+                ))}
+            </Slider>
+          </div>
+        </>
+        
          
-          <div className="space-y-6">
-         
-         <ul className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-5 gap-4">
-          {data.filter(a => a.role === 'survivor').
-          slice(0,5).
-          map((item,index) => (
-          <CardUI key={index} item={item} />
-          ))}
-        </ul>
-
-        <ul className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-5 gap-4">
-        {data.filter(a => a.role === 'killer').
-        slice(0,5).
-        map((item,index) => (
-        <CardUI key={index} item={item} />
-        ))}
-        </ul>
-        </div>
-       
-
       ) : (
-        <CircularProgressUI />
+      <div className='absolute inset-0 flex items-center justify-center'>
+                <CircularProgressUI />
+              </div>
       )}
 
-    </div>
+   
     </section>
    
   
